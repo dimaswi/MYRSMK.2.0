b43c2725179a3048978ff1f6063263739a9e664c
Dimas Wisnu Wirawan <dimasw347@gmail.com>
2023-12-20 07:07:00 +0700

    tambahkan gate admin dan juga perbaiki paginate

--- app/Http/Livewire/Admin/IPAddress.php
+++ app/Http/Livewire/Admin/IPAddress.php
@@ -5,16 +5,21 @@ namespace App\Http\Livewire\Admin;
 use App\Models\Stat;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Support\Facades\Gate;
 
 class IPAddress extends Component
 {
     use WithPagination;
-    public $perPage;
+    public $perPage = 5;
 
     public function render()
     {
+        if (! Gate::allows('manage_users')) {
+            return abort(401);
+        }
+
         return view('livewire.admin.i-p-address',[
-            'stats' => Stat::paginate(),
+            'stats' => Stat::paginate($this->perPage),
         ]);
     }
 }
